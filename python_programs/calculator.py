792b59d34dca345146d1adeac4dfaa9f3cf58aa5
larrybah <larrybah@gmail.com>
2025-08-02 06:29:52 +0000

    redone

--- python_programs/calculator.py
+++ python_programs/calculator.py
@@ -1,29 +1,22 @@
 #!/usr/bin/env python3
 
 """
-A simple calculator program.
+A Simple Calculator program that handles basic arithmetic.
 """
 
 first_number = int(input("Enter first number: "))
-
-operator = input("Operator: +, -, *, /: ")
-
+operator = input("Enter the Operator (+, -, /, *): ")
 second_number = int(input("Enter second number: "))
-
-result = 0
 
 if operator == '+':
     result = first_number + second_number
-    print(result)
+    print(f"Result: {result}")
 elif operator == '-':
     result = first_number - second_number
-    print(result)
+    print(f"Result: {result}")
+elif operator == '/':
+    result = first_number / second_number
+    print(f"Result: {result}")
 elif operator == '*':
     result = first_number * second_number
-    print(result)
-elif operator == '/':
-    if first_number == 0 or second_number == 0:
-        print("Division by Zero is not allowed!")
-    else:
-        result = first_number / second_number
-        print(result)
+    print(f"Result: {result}")
