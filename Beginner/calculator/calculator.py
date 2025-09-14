9c9c0c119813279f548b3f54d6e85b2530cfe6de
larrybah <larrybah@gmail.com>
2025-09-14 12:51:22 +0000

    first attempt

--- Beginner/calculator/calculator.py
+++ Beginner/calculator/calculator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 """
+First Attempt:
 Accept user input and calculate result base on the operator provided.
 """
 
@@ -8,20 +9,20 @@ first_number = int(input("Enter first number: "))
 operator = input("Enter operator(+,-,/,*): ") 
 second_number = int(input("Enter second number: "))
 
-operators = ['+', '-', '*', '/']
-
-for operator in operators:
-    if operator == '+':
-        result = first_number + second_number
-        print(f"{first_number} + {second_number} = {result}")
-    elif operator == '-':
-        result = first_number - second_number
-        print(f"{first_number} + {second_number} = {result}")
-    elif operator == '/':
+if operator == '+':
+    result = first_number + second_number
+    print(f"{first_number} + {second_number} = {result}")
+elif operator == '-':
+    result = first_number - second_number
+    print(f"{first_number} + {second_number} = {result}")
+elif operator == '/':
+    if second_number == 0:
+        print("Cannot Divide by Zero!")
+    else:
         result = first_number / second_number
         print(f"{first_number} / {second_number} = {result}")
-    elif operator == '*':
-        result = first_number * second_number
-        print(f"{first_number} * {second_number} = {result}")
-    else:
-        print("Enter a valid Operator!")
+elif operator == '*':
+    result = first_number * second_number
+    print(f"{first_number} * {second_number} = {result}")
+else:
+    print("Enter a valid Operator(+,-,*,/)!")
