0bc8460e2ad60c98018f3453ed0bb9adea19e4df
larrybah <larrybah@gmail.com>
2025-09-14 12:57:45 +0000

    second attemp on calc program

---
+++ Beginner/calculator/02-calculator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+Second Attempt:
+    calculator program that handles basic arithmetic 
+"""
+
+def user_input(first_number, operator, second_number):
+    first_number = input("Enter first number: ")
+    operator = input("Enter operator: ")
+    second_number: input("Enter second number: ")
